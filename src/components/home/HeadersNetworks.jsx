c6ec879784246060e3c8a689e7f608372bbb080b
LovroPopovic <lovropop@gmail.com>
2023-03-16 02:37:01 +0100

    peti

---
+++ src/components/home/HeadersNetworks.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const HeadersNetworks = () => {
+  return (
+    <div className='home_networks'>
+        <a href=' ' className='home_network_link' target="_blank">
+        <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href='google.com' className='home_network_link' target="_blank">
+        <i class="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a href='google.com' className='home_network_link' target="_blank">
+        <i class="fa-brands fa-square-github"></i>
+        </a>
+
+        <a href='google.com' className='home_network_link' target="_blank">
+        <i class="fa-brands fa-twitter"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeadersNetworks
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./home.css"
+import Avatar from "../../assets/avatar.png"
+import HeadersNetworks from './HeadersNetworks'
+import Scroll from './Scroll'
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className='home container' id="home">
+      <div className='Intro'>
+        <img src={Avatar} alt="" className="home_img" />
+        <h1 className="home_name">Lovro Popovic</h1>
+        <span className="home_ed"> I'm a Front-End developer. </span>
+        <HeadersNetworks/>
+        <a href="#contact" className="btn">Hire Me</a>
+        <Scroll/>
+      </div>
+    </section>
   )
 }
 
