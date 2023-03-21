61c9634b493fc109fa38e7fae632a0c268f9efdb
LovroPopovic <lovropop@gmail.com>
2023-03-21 18:50:10 +0100

    11

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from "./components/home/Home"
 import Portfolio from "./components/portfolio/Portfolio"
 import Resume from "./components/resume/Resume"
 import Services from "./components/services/Services"
-import Sidebar from "./components/sidebar/sidebar"
+import Sidebar from "./components/sidebar/Sidebar"
 
 
 
