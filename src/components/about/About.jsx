a4078c39b1134eee9a65a7363ccb46fab23ca81e
LovroPopovic <lovropop@gmail.com>
2023-03-22 22:15:04 +0100

    12

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="about container section" id="about">
+      <h2 className="section_title">About Me</h2>
+      <div className="about_container grid">
+        <img src="" alt="" className="about_img" />
+        <div className="about_data grid">
+          <div className="about_info">
+            <p className="about_description">I'm Lovro Popovic, a web developer and informatics and digital technologies student based in Rijeka, Croatia. 
+            I specialize in front-end and back-end development, and have a strong foundation in building websites using a variety of tools and technologies. 
+            As a student, I'm dedicated to expanding my knowledge and staying up-to-date with the latest trends in web development. I'm committed to creating 
+            engaging and innovative websites that provide exceptional user experiences.</p>
+            <a href=" " className="btn">Download CV</a>
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
--- src/components/home/HeadersNetworks.jsx
+++ src/components/home/HeadersNetworks.jsx
@@ -3,10 +3,6 @@ import React from 'react'
 const HeadersNetworks = () => {
   return (
     <div className='home_networks'>
-        <a href=' ' className='home_network_link' target="_blank">
-        <i className="fab fa-instagram"></i>
-        </a>
-
         <a href='google.com' className='home_network_link' target="_blank">
         <i className="fab fa-linkedin"></i>
         </a>
